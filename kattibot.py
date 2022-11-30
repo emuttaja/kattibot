305e820073988fa4fded21bf1c8760d6c6330e5a
ahokase <elias.ahokas@tuni.fi>
2022-11-30 17:45:43 +0200

    imlement basic functionality

---
+++ kattibot.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+
+import logging, datetime, pytz, requests
+
+from telegram import (
+    Update,
+    ParseMode
+)
+
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    MessageHandler,
+    CallbackContext,
+    MessageFilter,
+)
+
+#define timezones
+FIN = pytz.timezone("Europe/Helsinki")
+
+
+CHATS = [1962469908]
+
+
+#define handlers
+
+def start(update:Update, context:CallbackContext):
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text= "Hii!"
+    )
+
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text= "@elijjjas is my creator. Message him instead."
+    )
+
+def send_cat_to_groups(context:CallbackContext):
+    """Get an ai generated picture of a random cat from
+    thiscatdoesnotexist.com and send it to chats defined
+    in CHATS
+
+    Parameters
+    ----------
+    context : CallbackContext
+        bot context
+    """
+
+    #download the cat
+    url = "https://thiscatdoesnotexist.com/"
+    img_data = requests.get(url).content
+
+    #send the cat
+    for chat in CHATS:
+        # in theory we could just send a link to the image's location and telegram 
+        # would display it but telegram's caching would not update the image 
+        # thus the image would be the same every time
+        context.bot.send_photo(chat_id=chat, photo=img_data)
+    
+def send_cat(update:Update, context:CallbackContext):
+    """Get an ai generated picture of a random cat from
+    thiscatdoesnotexist.com and send it to the user
+
+    Parameters
+    ----------
+    context : CallbackContext
+        bot context
+    """
+
+    #download the cat
+    url = "https://thiscatdoesnotexist.com/"
+    img_data = requests.get(url).content
+
+    #send the cat
+    
+    context.bot.send_photo(chat_id=update.effective_chat.id, photo=img_data)
+
+
+def main():
+    #load api key
+    with open("api_key.txt", "r") as file:
+        api_key = file.readline()
+    
+
+    #define bot updater
+    updater = Updater(api_key)
+    dispatcher = updater.dispatcher
+
+    #add handlers
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("katti", send_cat))
+
+    #start job queue
+    job = updater.job_queue
+
+    #add jobs
+    cat_time = datetime.time(hour=17, minute=43, tzinfo=FIN)
+    daily_cat = job.run_daily(send_cat_to_groups, cat_time)
+
+    #main loop
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == "__main__":
+    main()
